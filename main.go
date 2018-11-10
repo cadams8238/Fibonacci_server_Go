718d06c6d766b7d02779931d0e076625c75889b5
Courtney Adams <cadams8238@gmail.com>
2018-11-10 13:51:11 -0800

    it's alivegit diff I mean...Fib api is functional. Added a helpful message for home endpoint at  and added fibonacci functions to compute sequence and handle endpoint at

--- main.go
+++ main.go
@@ -1,25 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+func GoToIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Oops, you're in the wrong place. To access the fibonacci API, navigate to `/api` endpoint.\n")
+}
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+	fmt.Fprint(w, "Welcome!\n\nAvailable endpoints:\n`/api` -> Home\n`/api/fibonacci/:num` -> Returns Fibonacci sequence in JSON (ie: /api/fibonacci/6 returns {[0,1,1,2,3,5]})")
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
+func fibonacciSeq(num int) []uint64 {
+	if num == 0 {
+		return []uint64{0}
+	}
+
+	if num == 1 {
+		return []uint64{0}
+	}
+
+	sequence := []uint64{0, 1}
+	for i := 2; i < num; i++ {
+		sequence = append(sequence, sequence[i-1]+sequence[i-2])
+	}
+	return sequence
+}
+
+func FibonacciHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	num, err := strconv.Atoi(ps.ByName("num"))
+
+	if err != nil {
+		http.Error(w, "Please enter a valid number.", http.StatusInternalServerError)
+		return
+	}
+
+	fib, err := json.Marshal(fibonacciSeq(num))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(fib)
+}
+
 func main() {
 	router := httprouter.New()
+	router.GET("/", GoToIndex)
 	router.GET("/api", Index)
 	router.GET("/api/hello/:name", Hello)
+	router.GET("/api/fibonacci/:num", FibonacciHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
