9ef47046aa773ddf78e2e487800a8f57e9157b3a
Courtney Adams <cadams8238@gmail.com>
2018-11-10 11:22:27 -0800

    touring go and figuring out how to make packages and run go files

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/cadams8238/stringutil"
+)
 
 func main() {
-  fmt.Println("Hello, world.")
+  fmt.Println(stringutil.Reverse("!oG, olleH"))
 }
