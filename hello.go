c183fb77b89a949ba456111cd5b533374e9fb0dc
Courtney Adams <cadams8238@gmail.com>
2018-11-10 12:47:39 -0800

    added boilerplate code for simple api with home message at  and a Hello ,Go! message at

--- hello.go
+++ hello.go
@@ -1,10 +1,12 @@
+// just a test file to learn and get introduced to Go
 package main
 
 import (
-  "fmt"
-  "github.com/cadams8238/stringutil"
+	"fmt"
+
+	"github.com/cadams8238/stringutil"
 )
 
 func main() {
-  fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
 }
